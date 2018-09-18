a5fa3706f5a6bc36f0fe8d02f04a0dd595b80c9a
Halim Lais <halimlais@hotmail.com>
2018-09-18 16:38:27 +0100

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (PlayerController))]
+public class Player: MonoBehaviour
+{
+    public float moveSpeed = 50;
+    PlayerController controller;
+    Camera viewCamera;
+
+    private void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        viewCamera = Camera.main;
+    }
+    private void Update()
+    {
+
+        Vector3 moveInput = new Vector3(CrossPlatformInputManager.GetAxisRaw("Horizontal"), 0, CrossPlatformInputManager.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, 0f);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 pointOfIntersection = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
+            controller.LookAt(pointOfIntersection);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 m_velocity;
+    Rigidbody m_rigidBody;
+
+    public void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_rigidBody.freezeRotation = true;
+
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        m_velocity = velocity;
+        
+    }
+    public void FixedUpdate()
+    {
+        m_rigidBody.MovePosition(m_rigidBody.position + m_velocity * Time.fixedDeltaTime);
+    }
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+}
